00bf08e5afa694446dbb8352df0fc4673c975fd5
jordi <jordi>
2007-03-27 05:40:53 +0000

    Fixed the license text, now it has the proper FSF address

--- src/tensor_TYPE.h
+++ src/tensor_TYPE.h
@@ -1,6 +1,6 @@
 /* tensor/tensor_NAME.h
  *
- * Copyright (C) 2003, 2004 Jordi Burguet-Castell
+ * Copyright (C) 2003, 2004, 2007 Jordi Burguet-Castell
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
